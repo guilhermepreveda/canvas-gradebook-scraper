c215dc0d50132ce1aa0dfdd5de461d67b44b1400
Guilherme Preveda Milék <guilhermeprevedamilek@gmail.com>
2022-07-21 23:21:27 -0300

    update API to the new version of the canvas

--- src/utils/Puppeteer.js
+++ src/utils/Puppeteer.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import fetch from "node-fetch";
 
 import AppError from "../errors/AppError.js";
 import CanvasApi from "../api/Canvas.api.js";
@@ -9,7 +10,7 @@ export default class Puppeteer {
   static selectors = {
     USERNAME_SELECTOR: "#pseudonym_session_unique_id",
     PASSWORD_SELECTOR: "#pseudonym_session_password",
-    CTA_SELECTOR: "Button.Button--login",
+    CTA_SELECTOR: ".Button.Button--login",
     DOWNLOAD_BUTTON_SELECTOR: "#content > div > span > a",
   };
 
@@ -69,7 +70,7 @@ export default class Puppeteer {
 
       // console.log("8. Finding the _normandy_session cookie");
       const _normandy_session = cookies.find(
-        ({ name }) => name === "_normandy_session"
+        ({ name }) => name === "_normandy_session" || "_legacy_normandy_session"
       );
 
       // console.log("9. Checking if the _normandy_session cookie exists");
@@ -93,33 +94,39 @@ export default class Puppeteer {
     }
   }
 
-  static async gradebookCSVRequest(page, course_id) {
-    // console.log("==== gradebookCSVRequest(page, course_id) ==========");
-
+  static async gradebookCSVRequest(course_id) {
     try {
       // console.log("1. Going to gradebook CSV request page");
-      await page.goto(`${canvasUrl}/courses/${course_id}/gradebook_csv`);
+      const response = await fetch(
+        `${canvasUrl}/courses/${course_id}/gradebook_csv`,
+        {
+          headers: {
+            accept:
+              "application/json+canvas-string-ids, application/json, text/plain, */*",
+            "accept-language": "pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+            "content-type": "application/json;charset=UTF-8",
+            "sec-ch-ua":
+              '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-csrf-token":
+              "hcjM9nWxcV6v/V2FVBl/AWejowUTTpASGRh/qkBq35C9poauNtgrHZfJMNcyeDxlLenZcld8yWttYRLfBQWR4g==",
+            "x-requested-with": "XMLHttpRequest",
+            cookie:
+              "_legacy_normandy_session=vcRHoOREJCFwVLSOOrD8fA+yRuOHXtTyVuQDm9VeFngKBOCZdCjXJBQPkBN7nRGyP4jWkcE3Lzjbwa82qhHn98jSQ5qW8Rau5R0Aiy8Iu0zjCQDlfsKlSHzBlucPsWSDKbLj3Ied1xvwmAy1ZDiXRAigaT6acvu8v7F9Prazjd6OpUt3Wq8WCP82M8kJN8fwPT9IETr4kXs1Wan8UqdTYYFpNTUWPPYHMK33Uw5JoNonp0RYYrtxm_2PCjaDUdjo6I_35xn8cMXxM5uL8b4YIuAwK8V_adlz1jIJVS5s0ffIFUDpHG8zhc6285wnoDyRkts3fs-_szH7ztyVgFqHVAi_3B9A6ke0cwOPnS5h5jbuq1CxJ8PkAjka8sNfbj7YfzvWtTKbQkC6S5Yjr9xc6IkiQbIfPVFL8Fmom8p4DTQor_nRHB_8jGT_GYgjhK7Lay7OJwd81KRTQ-2kq0thM22Xfojz1qxwK2KZqncmwEsqYl9SbUDRRXR9utr0nV1YydpVEHPqXV4C3ng3BqnznK_.TilF6UUgHwcsslHx_BXXhslP680.YtoDkw; _csrf_token=hcjM9nWxcV6v%2FV2FVBl%2FAWejowUTTpASGRh%2FqkBq35C9poauNtgrHZfJMNcyeDxlLenZcld8yWttYRLfBQWR4g%3D%3D",
+            Referer: `${canvasUrl}/courses/${course_id}/gradebook`,
+            "Referrer-Policy": "no-referrer-when-downgrade",
+          },
+          method: "POST",
+        }
+      );
 
-      // console.log("2. Evaluating gradebook CSV request page");
-      const requestJSON = await page.evaluate(() => {
-        // console.log("2.1. Selecting and formatting the page response");
+      const responseJSON = await response.json();
 
-        // 1 => while(1);{"attachment_id":25684,"progress_id":2253}
-        // 2 => ["while(1)", "{"attachment_id":25684,"progress_id":2253}"]
-        // 3 => "{"attachment_id":25684,"progress_id":2253}"
-        const response = document.querySelector("pre").innerText.split(";")[1];
-
-        // console.log("2.2. Converting and returning the page response to JSON formatting");
-
-        // 4 => {"attachment_id":25684,"progress_id":2253}
-        return JSON.parse(response);
-      });
-
-      // console.log("3. Taking a screenshot of the page");
-      await page.screenshot({ path: "./src/screenshots/csvRequestPage.png" });
-
-      // console.log("4. Returning requestJSON.attachment_id");
-      return requestJSON.attachment_id;
+      return responseJSON.attachment_id;
     } catch (err) {
       // console.log("> An error has occurred");
       throw new AppError("Course not found", 404);
